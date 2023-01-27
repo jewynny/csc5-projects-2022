7a12486c8d4ba01ca6a0023242ca6f1e32853640
Janelle Gwynn M. Hamoy <122710712+jewynny@users.noreply.github.com>
2023-01-26 23:15:23 -0800

    Update LoadArraysFromFile.cpp

--- Project 04/LoadArraysFromFile.cpp
+++ Project 04/LoadArraysFromFile.cpp
@@ -1,21 +1,21 @@
 /**************************************************************************
 * AUTHOR : Janelle Gwynn M. Hamoy                                         *
-* PROJECT #4 : Parallel Arrays							  				  *
-* CLASS : CSC5 															  *
-* SECTION : MW 2:20PM - 5:30PM 											  *
-* DUE DATE : 5/25/22 													  *
+* PROJECT #4 : Parallel Arrays						  *
+* CLASS : CSC5 								  *
+* SECTION : MW 2:20PM - 5:30PM 						  *
+* DUE DATE : 5/25/22 							  *
 *************************************************************************/
 
 #include "myheader.h"
 
 /********************************************************************
- * Definition of LoadArraysFromFile  								*
+ * Definition of LoadArraysFromFile  				    *
  * This function will load the information from the input file into *
- * arrays.															*
+ * arrays.						 	    *
  ******************************************************************/
 
 void LoadArraysFromFile(ifstream &inFile, string name[], int idNum[],
-						float balances[], const int SIZE)
+			float balances[], const int SIZE)
 {
 	for (int count = 0; count < SIZE; count++)
 	{
