7e401487d392aeded3d6b6d36bfd28fdf7c16744
Janelle Gwynn M. Hamoy <122710712+jewynny@users.noreply.github.com>
2023-01-26 23:18:36 -0800

    Update PrintReportToFile.cpp

--- Project 04/PrintReportToFile.cpp
+++ Project 04/PrintReportToFile.cpp
@@ -1,33 +1,33 @@
 /**************************************************************************
 * AUTHOR : Janelle Gwynn M. Hamoy                                         *
-* PROJECT #4 : Parallel Arrays							  				  *
-* CLASS : CSC5 															  *
-* SECTION : MW 2:20PM - 5:30PM 											  *
-* DUE DATE : 5/25/22 													  *
+* PROJECT #4 : Parallel Arrays						  *
+* CLASS : CSC5 								  *
+* SECTION : MW 2:20PM - 5:30PM 						  *
+* DUE DATE : 5/25/22 							  *
 *************************************************************************/
 
 #include "myheader.h"
 
 /********************************************************************
- * Definition of PrintReport to File	 							*
- * This function will output the completed report to the output 	*
- * file. The completed report will include the employee number, 	*
- * name, and balance. This function will only use the employees the	*
+ * Definition of PrintReport to File	 			    *
+ * This function will output the completed report to the output     *
+ * file. The completed report will include the employee number,     *
+ * name, and balance. This function will only use the employees the *
  * user has searched. The employees that were not found will not be *
- * output. This function will also output the average balance due 	*
- * from the employees.												*
+ * output. This function will also output the average balance due   *
+ * from the employees.						    *
  ******************************************************************/
 
 void PrintReportToFile(ofstream &outFile, int positon, string name[],
-					   int idNum[], float balances[], int match[],
-					   int counter, const int SIZE)
+		       int idNum[], float balances[], int match[],
+		       int counter, const int SIZE)
 {
-	float  totalBalance = 0;	//INPUT  - Total balance due from employees
-	float  average;				//INPUT  - Temporarily holds the subscript
-								//		   with found employee position
-								//		   in array
-	int    tempPos;				//OUTPUT - The average balance due from
-								//		   employees
+	float  totalBalance = 0;  //INPUT  - Total balance due from employees
+	float  average;		  //INPUT  - Temporarily holds the subscript
+				  //	     with found employee position
+				  //	     in array
+	int    tempPos;		  //OUTPUT - The average balance due from
+				  //	     employees
 
 	outFile << endl;
 	outFile << left << setw(9) <<  "ID #" << setw(25) << "NAME";
